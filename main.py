848bb2da98e710b9bffdefa8a5c4ec62a4a057a3
Rohan Patankar <rohanpatankar926@gmail.com>
2022-06-21 16:43:33 +0530

    Updated

--- main.py
+++ main.py
@@ -209,4 +209,4 @@ def trainRouteClient():
 port = int(os.getenv("PORT", 5000))
 if __name__ == "__main__":
     # port = 5000
-    app.run(host='127.0.0.1',port=5000, debug=True)
+    app.run(host='0.0.0.0',port=5000, debug=False)
