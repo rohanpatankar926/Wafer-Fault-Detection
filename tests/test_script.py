69b66441186168069a1f6ab051c56251e8e71bf8
Rohan Patankar <rohanpatankar926@gmail.com>
2022-06-21 17:36:16 +0530

    COmmit

--- tests/test_script.py
+++ tests/test_script.py
@@ -1,5 +1,4 @@
 import unittest
-
 from main import app
 import os
 
@@ -17,52 +16,37 @@ class TestToPerform(unittest.TestCase):
         print(response)
         self.assertEqual(response.status_code, 200)
 
-    def test_page(self):
+    def test_page1(self):
         response1 = self.app.get('/main', follow_redirects=True)
         print(response1)
         self.assertEqual(response1.status_code, 200)
         
 
-    def test_page(self):
+    def test_page2(self):
         response2 = self.app.get('/data', follow_redirects=True)
         print(response2)
         self.assertEqual(response2.status_code, 200)
     
-    def test_page(self):
+    def test_page3(self):
         response3 = self.app.get('/saved_models', follow_redirects=True)
         print(response3)
         self.assertEqual(response3.status_code, 200)
     
-    def test_page(self):
+    def test_page4(self):
         response3 = self.app.get('/upload', follow_redirects=True)
         print(response3)
         self.assertEqual(response3.status_code, 200)
     
-    def test_page(self):
+    def test_page5(self):
         response4 = self.app.get('/logs', follow_redirects=True)
         print(response4)
         self.assertEqual(response4.status_code, 200)
         
-    def test_page(self):
+    def test_page6(self):
         response5 = self.app.get('/stream/train', follow_redirects=True)
         print(response5)
         self.assertEqual(response5.status_code, 200)
         
-    def test_page(self):
-        response6 = self.app.get('/predict', follow_redirects=True)
-        print(response6)
-        self.assertEqual(response6.status_code, 200)
-        
-    def test_page(self):
-        response7 = self.app.get('/train', follow_redirects=True)
-        print(response7)
-        self.assertEqual(response7.status_code, 200)
-        
-    
-    
-
-
-
 
 if __name__ == '__main__':
     unittest.main()
